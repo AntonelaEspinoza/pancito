acef8587880af5754c57421ba8e083f0196c68f7
Antonella <antonelaespinoza665@gmail.com>
2025-08-05 16:59:07 -0300

    empezamos eventos

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+console.log
+('======================');
+console.log("Tarea 02 _ Eventos");
+
+console.log 
+('=====================================');
+
+// Evento onclick
+// este evento se dispara cuando el user hace click en un elemento
+function enArchivo() {
+        alert("Adios, cerraste sesion");
+}
